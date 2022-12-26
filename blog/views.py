9e32bfaff4dc6241dfb821033ac1b9e7b22fb164
Alexander <alexzubkoff123@gmail.com>
2022-12-27 00:56:30 +0200

    Added basic search engin for blog

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,9 @@ from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, get_object_or_404
 from .models import Post,  Comment
 from django.views.generic import ListView
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm,  SearchForm
 from django.core.mail import send_mail
+from django.contrib.postgres.search import SearchVector
 
 
 class PostListView(ListView):
@@ -86,3 +87,23 @@ def post_share(request, post_id):
         }
     )
 
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = Post.published.annotate(
+                search=SearchVector('title', 'body'),
+            ).filter(search=query)
+    return render(
+        request,
+        'blog/post/search.html',
+        {'form': form,
+         'query': query,
+         'results': results}
+    )
